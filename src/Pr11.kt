07d02688be36f7241eb97ac61ab3703603af2537
Sakariya Krish <sakariyakrish23@gmail.com>
2026-06-23 08:16:15 +0530

    init commit

---
+++ src/Pr11.kt
@@ -0,0 +1,32 @@
+fun main(){
+    var i=10
+    var j:Int=20
+    var k:Char='u'
+    var l:String= "abcd 123"
+    var m:Boolean= true
+    var n:Float=5.6f
+    var o:Long=85242394
+    var p:Short=-3
+    var q:Byte=54
+
+    val x=100
+    j=200
+    val a="Krish"
+    val b = 350000
+    val c = 5.55951212
+
+    println("i="+i)
+    println("j="+j)
+    println("k="+k)
+    println("l="+l)
+    println("m="+m)
+    println("n="+n)
+    println("o="+o)
+    println("p="+p)
+    println("q="+q)
+    println("x="+x)
+    println("My Name is $a")
+    println("Car Price is: ${b * 12}")
+    println(String.format("c: %.2f", c))
+
+}
---
+++ src/pr12.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a: Int = 10
+    val b: Double = a.toDouble()
+
+    val c: String="1000"
+    val d: Int = c.toInt()
+    val e: Double = c.toDouble()
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+
+}`
