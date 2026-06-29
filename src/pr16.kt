baafea4b47e79de33b114f13c5d3861591b9d7cc
Sakariya Krish <sakariyakrish23@gmail.com>
2026-06-30 01:54:11 +0530

    init

---
+++ src/pr16.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val number1 = readln().toInt()
+    val number2 = readln().toInt()
+    println(add(number1, number2))
+    println(sub(number1, number2))
+    println(mul(number1, number2))
+    println(div(number1, number2))
+}
+ fun add(number1: Int, number2: Int): Int {
+     return number1 + number2
+ }
+
+fun sub(number1: Int, number2: Int): Int {
+    return number1 - number2
+}
+
+fun mul(number1: Int, number2: Int): Int {
+    return number1 * number2
+}
+
+fun div(number1: Int, number2: Int): Int {
+    return number1 / number2
+}
